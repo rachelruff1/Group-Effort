aba7ec99ff15f0ce8f620e26a5356f6898c24da6
Kara Rainey <karain82@hotmail.com>
2018-04-02 14:31:41 -0500

    set up reducer for MyTripCard

---
+++ src/Components/CreateTrip/CreateTrip.js
@@ -0,0 +1,27 @@
+import React, {Component} from "react";
+import axios from 'axios';
+import connect from 'react-redux';
+
+class CreateTrip extends Component{
+    constructor(props){
+        super(props);
+
+
+        //bindings
+    }
+
+    //componentDidMount?
+
+    //handlers
+
+render(){
+    return(
+        <div>
+            <main>
+                <h2>You have no trips planned... you should do something about that...</h2>
+            </main>
+        </div>
+    )
+  }
+}
+export default CreateTrip;

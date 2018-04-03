8baa0e93ef9b65746c9b6350e3ea4b9088b559a8
Garrett <Garrettcrg@gmail.com>
2018-04-03 09:42:04 -0500

    added-location-component

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import Geosuggest from "react-geosuggest";
-
+import axios from "axios";
 class Search extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div className="Search">
-        <p>Search</p>
         <Geosuggest onSuggestSelect={sug => console.log(sug)} />
 
         <button> Go </button>

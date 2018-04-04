f3f4d557eab9fbdbe037df3a13d2584c5f2a0ee8
Garrett <Garrettcrg@gmail.com>
2018-04-04 09:07:23 -0500

    changed-redux-for-header

--- server/index.js
+++ server/index.js
@@ -111,15 +111,13 @@ app.get("/api/getProfile", (req, res) => {
       res.status(200).json(response);
     });
 });
-app.get('/api/getCities/:tripid', ctrl.getCities);
-app.get('/api/getTrip', ctrl.getTrip);
-app.get('/api/getSaved', ctrl.getSaved);
-app.get('/api/getFood', ctrl.getFood);
-app.get('/api/getThingsToDo', ctrl.getThingsToDo);
-app.get('/api/getMuseums', ctrl.getMuseums);
-app.get('/api/getFacts', ctrl.getFacts);
-
-
+app.get("/api/getCities/:tripid", ctrl.getCities);
+app.get("/api/getTrip", ctrl.getTrip);
+app.get("/api/getSaved", ctrl.getSaved);
+app.get("/api/getFood", ctrl.getFood);
+app.get("/api/getThingsToDo", ctrl.getThingsToDo);
+app.get("/api/getMuseums", ctrl.getMuseums);
+app.get("/api/getFacts", ctrl.getFacts);
 
 //------------- end of endpoints ----------------
 app.listen(port, () => {

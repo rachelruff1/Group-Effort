2c76737f37001e1c597049f082e49628ae31c370
Jordan Oakes <jordanoakes@Jordans-MacBook-Air.local>
2018-04-06 10:23:47 -0500

    changes

--- src/Components/Header/AppHeader/AppHeader.js
+++ src/Components/Header/AppHeader/AppHeader.js
@@ -9,40 +9,33 @@ import { Link } from "react-router-dom";
 import test2 from '../../Logo/trippie_logo.png';
 import Login from '../../Auth/Auth';
 class AppHeader extends Component {
-  constructor(props) {
-    super(props);
-  }
-  componentDidMount() {
-    this.props.getProfile();
-  }
-  render() {
-    console.log(this.props)
-    return (
-      <header className="app-header">  
-      {this.props.picture && <img className="user-photo"  src={this.props.picture} /> }
-      {/* <Logo/>   */}
-      <Link to="/Auth">
-          <button className="authbutton">Login</button>
-        </Link>
-        </header>,
-      <header className="app-header">
-      {this.props.picture && <img className="user-photo" src={this.props.picture} />}
-      <Login/>
-      <Link to='/'><img className="logo" src={test2} alt="logo" /></Link>
-        <div className="navbar">
-          <div className="responsive-nav">
-            <AppDrawer />
-          </div>
-        </div>
-      </header>
-    );
-  }
+ constructor(props) {
+   super(props);
+ }
+ componentDidMount() {
+   this.props.getProfile();
+ }
+ render() {
+   console.log(this.props)
+   return (
+     <header className="app-header">
+     {this.props.picture && <img className="user-photo" src={this.props.picture} />}
+     <Login/>
+     <Link to='/'><img className="logo" src={test2} alt="logo" /></Link>
+       <div className="navbar">
+         <div className="responsive-nav">
+           <AppDrawer />
+         </div>
+       </div>
+     </header>
+   );
+ }
 }
 function mapStateToProps(state) {
-  return {
-    picture: state.reducer1.picture
-  };
+ return {
+   picture: state.reducer1.picture
+ };
 }
 export default connect(mapStateToProps, {
-    getProfile
+   getProfile
 })(AppHeader);

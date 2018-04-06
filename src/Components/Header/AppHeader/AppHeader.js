fcaadebdc23b112ddec0ba8a3febe4978e7ec91e
Garrett <Garrettcrg@gmail.com>
2018-04-06 11:35:17 -0500

    d

--- src/Components/Header/AppHeader/AppHeader.js
+++ src/Components/Header/AppHeader/AppHeader.js
@@ -6,49 +6,44 @@ import Search from "../../Search/Search";
 import { getProfile } from "../../../ducks/reducer1";
 import AppDrawer from "./Drawer.js";
 import { Link } from "react-router-dom";
-import test2 from '../../Logo/trippie_logo.png';
-import Login from '../../Auth/Auth';
-import MenuExampleSimple from './dropdown';
+import test2 from "../../Logo/trippie_logo.png";
+import Login from "../../Auth/Auth";
+import MenuExampleSimple from "./dropdown";
 
 class AppHeader extends Component {
   constructor(props) {
     super(props);
     this.state = {
       toggle: false
-    }
+    };
     this.toggleDropDown = this.toggleDropDown.bind(this);
   }
   componentDidMount() {
     this.props.getProfile();
   }
 
-  toggleDropDown(){
+  toggleDropDown() {
     this.setState({
-      toggle: !(this.state.toggle)
-    })
+      toggle: !this.state.toggle
+    });
   }
   render() {
     console.log(this.state.toggle);
     return (
       <header className="app-header">
-<<<<<<< HEAD
-      <img className="user-photo" src={this.props.picture} onClick={()=>this.toggleDropDown()} />
-      
-      
-      {this.props.picture && <img className="user-photo" src={this.props.picture} onClick={()=>this.toggleDropDown()} />}
-      
-=======
-      {this.props.picture && <img className="user-photo" src={this.props.picture} />}
-      <div className="login-buttons">
->>>>>>> master
-      <Login/>
-      </div>
-      <Link to='/'><img className="logo" src={test2} alt="logo" /></Link>
+        {this.props.picture && (
+          <img className="user-photo" src={this.props.picture} />
+        )}
+        <div className="login-buttons">
+          <Login />
+        </div>
+        <Link to="/">
+          <img className="logo" src={test2} alt="logo" />
+        </Link>
         <div className="navbar">
           <div className="responsive-nav">
             <AppDrawer />
-            {this.state.toggle == true ?  <MenuExampleSimple /> : null}
-            
+            {this.state.toggle == true ? <MenuExampleSimple /> : null}
           </div>
         </div>
       </header>
@@ -61,5 +56,5 @@ function mapStateToProps(state) {
   };
 }
 export default connect(mapStateToProps, {
-    getProfile
+  getProfile
 })(AppHeader);

acc7b8277460fa0724460668bb1717eaee3f23e3
Jordan Oakes <jordanoakes@Jordans-MacBook-Air.local>
2018-04-09 12:16:48 -0500

    stuff

--- db/add_city_to_database.sql
+++ db/add_city_to_database.sql
@@ -1,1 +1,1 @@
-INSERT INTO city ( city_name, state, country, lat_lng, place_id, start_date, end_date, trip_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURN trip_id;
+INSERT INTO city ( city_name, state, country, lat_lng, place_id, start_date, end_date, trip_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING trip_id;
--- db/create_new_trip.sql
+++ db/create_new_trip.sql
@@ -1,1 +1,1 @@
-INSERT INTO trip ( trip_name, start_date, end_date, user_id ) VALUES ($1, $2, $3, $4) RETURN trip_id;
+INSERT INTO trip ( trip_name, start_date, end_date, user_id ) VALUES ($1, $2, $3, $4) RETURNING trip_id;

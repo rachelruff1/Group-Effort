8c6f519b980bf34634a673868e10ed0cff805a37
Kara Rainey <karain82@hotmail.com>
2018-04-12 15:31:23 -0500

    adding material ui buttons and correcting sizing

--- src/Components/ViewAllTrips/IndividualTripCard/IndividualTripCard.js
+++ src/Components/ViewAllTrips/IndividualTripCard/IndividualTripCard.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { deleteTrip } from "../../../ducks/reducer2";
 import {Link} from 'react-router-dom';
+import FlatButton from "material-ui/FlatButton";
 
 const IndividualTripCard = props => {
   console.log(props);
@@ -11,13 +12,12 @@ const IndividualTripCard = props => {
       <p>
         {props.city.start_date} - {props.city.end_date}
       </p>
-      <Link to={`/edit-trip/${props.city.trip_id}`}><button>Edit</button></Link>
-      <button onClick={() => {props.deleteTrip(props.city.trip_id); 
+      <Link to={`/edit-trip/${props.city.trip_id}`}><FlatButton label="+ Edit" labelStyle={{color: 'rgb(255, 255, 255)', fontSize: '75%'}} /></Link>
+      <FlatButton label="+ Delete" labelStyle={{color: 'rgb(255, 255, 255)', fontSize: '75%'}} onClick={() => {props.deleteTrip(props.city.trip_id); 
     //   props.toggle();
         window.location.reload();
-        }}>
-        Delete
-      </button>
+        }} />
+      
     </div>
   );
 };

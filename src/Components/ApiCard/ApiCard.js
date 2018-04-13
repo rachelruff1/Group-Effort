f1bef410dd7f6ab8cc9e2e8a8f79db2fb2859910
Jordan Oakes <jordanoakes@Jordans-MacBook-Air.local>
2018-04-13 15:52:35 -0500

    nothing

--- src/Components/ApiCard/ApiCard.js
+++ src/Components/ApiCard/ApiCard.js
@@ -40,20 +40,11 @@ class ApiCard extends Component {
   }
 
   render() {
-<<<<<<< HEAD
-      let styles = {
-        maxWidth: 255,
-        marginRight: 'auto',
-        
-      };
-      console.log(this.props.results);
-=======
     let styles = {
       maxWidth: 255,
       marginRight: "auto"
     };
     console.log(this.props.results);
->>>>>>> master
     return (
       <div className="Foodcards">
         <Card styles={styles}>
